696a29e036211fba184db97a5e189472cac320d2
9P1xel <re9pixel@gmail.com>
2023-06-06 20:20:55 +0300

    задача на сложение разность и вычитание

---
+++ main.py
@@ -0,0 +1,11 @@
+first_number = int(input("first numer: "))
+second_number = int(input("second number: "))
+
+answer = first_number + second_number
+print(answer)
+
+answer = first_number - second_number
+print(answer)
+
+answer = first_number * second_number
+print(answer)
