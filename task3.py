c76de5c735529f115cc94cce510cfa06e3090923
9P1xel <re9pixel@gmail.com>
2023-06-09 22:36:55 +0300

    квадрат чисел

--- task3.py
+++ task3.py
@@ -1,18 +1,10 @@
 list1 = []
+qt = 0
 for i in range(1,1001):
     square = i ** 2
-    list1.append(square)
-print(list1)
-qt = 0
-list2 = []
-for item in list1:
-    item = str(item)
-    list2.append(item)
-print(list2)
-for b in list2:
-    if '20' in b:
-        digit = (list2[b])
-        list2.remove(digit)
-        qt += 1
-print(list1)
-print("Количество удаленных чисел: ", qt)
+    if '20'  not in str(square):
+        list1.append(square)
+    else:
+        print(square)
+print(1000 - len(list1))
+
