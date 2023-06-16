d9f250d62e7a47c0bb148c0ed2d560becc8840b2
9P1xel <re9pixel@gmail.com>
2023-06-16 19:37:17 +0300

    dictionary2

--- task5.py
+++ task5.py
@@ -3,3 +3,8 @@ for key, value in dict1.items():
     print(key,'->',value)
 sum = sum(dict1.values())
 print(sum)
+
+dict1['f'] = None
+print(dict1)
+dict2 = [dict(dict1.items()) for value in dict1.values() if value != None]
+print(dict2)
