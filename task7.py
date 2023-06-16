a6df724babe4341ea4341f37686ccf6c1c895c23
9P1xel <re9pixel@gmail.com>
2023-06-16 21:57:32 +0300

    function

--- task7.py
+++ task7.py
@@ -1,8 +1,8 @@
-def digit(n):
-    if n>=0:
+def dgt(n):
+    if n>0:
         return "положительное"
     elif n==0:
         return "равно нулю"
     else:
         return "отрицательное"
-digit(3)
+dgt(3)
