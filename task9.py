7552972efd51a15e16b9b4ef51d700ee48059d11
9P1xel <re9pixel@gmail.com>
2023-06-23 20:00:45 +0300

    sort lower reg

--- task9.py
+++ task9.py
@@ -1,7 +1,14 @@
-symbols = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-strng = input()
-def remove_symbols(strng, symbols):
-    result = [i for i in strng if i not in symbols]
-    return print(''.join(map(str, result)))
+# symbols = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+# strng = input()
+# def remove_symbols(strng, symbols):
+#     result = [i for i in strng if i not in symbols]
+#     print(''.join(result))
+#
+# remove_symbols(strng, symbols)
 
-remove_symbols(strng, symbols)
+def srt():
+    words = [w.lower() for w in input().split(",")]
+    # words.sort()
+    # return words
+    return sorted(words)
+print(srt())
