c3fd79cfb10ce944c65b2f44d1cdae41a66e2961
9P1xel <re9pixel@gmail.com>
2023-06-30 19:25:56 +0300

    insects

--- task11.py
+++ task11.py
@@ -1,15 +1,26 @@
 # напишите функцию календарь, на вход год, месяц, и число, число(опциональный аргумент)
 
 '''def calendar(yyyy, mm, dd=''):
-    print('Today is {} {} {}'.format(dd, mm, yyyy))
+    print('Today is {}/{}/{}'.format(dd, mm, yyyy))
 
 
 calendar(1000, 12, 3)
 '''
 
-
-def insects(*args):
+'''def insects(*args):
     print(args)
-
-
+    
 insects('жесткокрылые', 'двукрылые', 'чешуекрылые', 'перепончатокрылые')
+'''
+
+
+def print_hello():
+    strng = "Привет питонист!"
+
+    def print_str(st):
+        print(st)
+
+    print_str(strng)
+
+
+print_hello()
