ff3b7b21fae1aee9c5eac5e31799931545727306
9P1xel <re9pixel@gmail.com>
2023-06-30 19:43:18 +0300

    nested func

--- task11.py
+++ task11.py
@@ -13,8 +13,7 @@ calendar(1000, 12, 3)
 insects('жесткокрылые', 'двукрылые', 'чешуекрылые', 'перепончатокрылые')
 '''
 
-
-def print_hello():
+'''def print_hello():
     strng = "Привет питонист!"
 
     def print_str():
@@ -23,4 +22,17 @@ def print_hello():
     print_str()
 
 
-print_hello()
+print_hello()'''
+
+strng = input()
+
+
+def letters_counter(st):
+    vowels = 'AaEeIiOoUuYy'
+    vowels_letters = [letters for letters in st if x in vowels]
+    consonants_letters = [letters for letters in st if x not in vowels]
+    print('vowels: ', len(vowels_letters))
+    print('consonants: ', len(consonants_letters))
+
+
+letters_counter(strng)
