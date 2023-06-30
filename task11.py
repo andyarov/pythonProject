ba75a403dce561fa0bfe822c6aa2a5ad38058fb2
9P1xel <re9pixel@gmail.com>
2023-06-30 19:07:29 +0300

    calendar

---
+++ task11.py
@@ -0,0 +1,7 @@
+# напишите функцию календарь, на вход год, месяц, и число, число(опциональный аргумент)
+
+def calendar(yyyy, mm, dd=''):
+    print('Today is {} {} {}'.format(dd, mm, yyyy))
+
+
+calendar(yyyy, mm, dd)
