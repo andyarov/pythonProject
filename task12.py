835aaf84b402c6fc664c037d2490d97303c32922
9P1xel <re9pixel@gmail.com>
2023-06-30 21:06:39 +0300

    roman to decimal

--- task12.py
+++ task12.py
@@ -2,8 +2,16 @@ tallies = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 
 def roman_to_decimal(roman_number):
-    result = [value for key, value in tallies.items() if key == roman_number]
-    print(result)
+    sum = 0
+    for i in range(len(roman_number) - 1):
+        left = roman_number[i]
+        right = roman_number[i + 1]
+        if tallies[left] < tallies[right]:
+            sum -= tallies[left]
+        else:
+            sum += tallies[right]
+    sum += tallies[roman_number[-1]]
+    return sum
 
 
-roman_to_decimal("I")
+print(roman_to_decimal("III"))
