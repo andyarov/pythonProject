46c327fffa382ecff5cf66eeaeee1241a5d9eac4
9P1xel <re9pixel@gmail.com>
2023-06-30 21:16:15 +0300

    class Rectangle

--- task12.py
+++ task12.py
@@ -1,17 +1,13 @@
-tallies = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+class Rectangle():
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def square(self, ):
+        sq = self.a * self.b
+        return sq
 
 
-def roman_to_decimal(roman_number):
-    sum = 0
-    for i in range(len(roman_number) - 1):
-        left = roman_number[i]
-        right = roman_number[i + 1]
-        if tallies[left] < tallies[right]:
-            sum -= tallies[left]
-        else:
-            sum += tallies[right]
-    sum += tallies[roman_number[-1]]
-    return sum
-
-
-print(roman_to_decimal("III"))
+rect = Rectangle(4, 5)
+print(rect.square())
