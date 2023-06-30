73e9fcdcdab59f759bb874ba25ffd2166fecca3d
9P1xel <re9pixel@gmail.com>
2023-06-30 19:57:30 +0300

    letters counter

--- task11.py
+++ task11.py
@@ -38,7 +38,14 @@ letters_counter(strng)'''
 '''Нужно вывести первые n строк треугольника Паскаля.
 В этом треугольнике на вершине и по бокам стоят единицы, 
 а каждое число внутри равно сумме двух расположенных над ним чисел.'''
-a = list(range(max(6, 0)))
-b = list(range(6))
-print(a)
-print(b)
+
+
+def pascal_triangle(n):
+    row = [1]
+    y = [0]
+    for x in range(max(n, 0)):
+        print(row)
+        row = [left + right for left, right in zip(row + y, y + row)]
+
+
+pascal_triangle(6)
