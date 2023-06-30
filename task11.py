5a7ed0ca5dcbbcd8e4263ca2286b368f282c6d07
9P1xel <re9pixel@gmail.com>
2023-06-30 21:08:55 +0300

    roman to decimal

--- task11.py
+++ task11.py
@@ -35,3 +35,21 @@ def letters_counter(st):
 
 
 letters_counter(strng)'''
+
+'''tallies = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+
+def roman_to_decimal(roman_number):
+    sum = 0
+    for i in range(len(roman_number) - 1):
+        left = roman_number[i]
+        right = roman_number[i + 1]
+        if tallies[left] < tallies[right]:
+            sum -= tallies[left]
+        else:
+            sum += tallies[right]
+    sum += tallies[roman_number[-1]]
+    return sum
+
+
+print(roman_to_decimal("III"))'''
