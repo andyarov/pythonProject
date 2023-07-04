3610b9ddde2df45ed2e2e42efbe2e039e1be1f25
9P1xel <re9pixel@gmail.com>
2023-07-04 19:48:00 +0300

    Freelance

--- task13.py
+++ task13.py
@@ -1,16 +1,16 @@
 class Employee:
-    emp_conunt = 0
+    emp_count = 0
 
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
-        Employee.emp_conunt += 1
+        Employee.emp_count += 1
 
     def display_employee(self):
         print("Name:", self.name, "salary:", self.salary)
 
     def display_count(self):
-        print("All ", Employee.emp_conunt)
+        print("All ", Employee.emp_count)
 
 
 empl = Employee("Tom", 50000)
